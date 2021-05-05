ad9c834c35adee10cc2ec3f6116ee3a1db6d12b0
gabrielcicero45 <gabrielcicero45@gmail.com>
2021-05-05 11:38:33 -0300

    Fixing Bug

--- myYTvideosApp/views.py
+++ myYTvideosApp/views.py
@@ -2,20 +2,18 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.http import FileResponse
 from pytube import YouTube
-
 import os
 import time
 
 # Create your views here.
 
 def index(request):
-    
     if request.method == 'POST':
         if request.POST['link']:
             link = request.POST['link']
             print(link)
             time.sleep(5)
-            return FileResponse(open(YouTube(link).streams.first().download(skip_existing=True),as_attachment=True,'rb'))
+            return FileResponse(open(YouTube(link).streams.first().download(skip_existing=True),'rb'))
         
     return render(request,'index.html')
 
